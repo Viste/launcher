a6e4c0fa628858aa26a12bc957ba01d913c167b0
Viste <viste02@gmail.com>
2020-08-09 20:42:38 +0300

    update readme and add some

---
+++ target/debug/deps/launcher-0453315868fd7e9e.d
@@ -0,0 +1,5 @@
+/home/viste/dev-wow/launcher/target/debug/deps/launcher-0453315868fd7e9e.rmeta: src/main.rs
+
+/home/viste/dev-wow/launcher/target/debug/deps/launcher-0453315868fd7e9e.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/launcher-7c8aeccff42c4303.d
@@ -0,0 +1,5 @@
+/home/viste/dev-wow/launcher/target/debug/deps/launcher-7c8aeccff42c4303.rmeta: src/main.rs
+
+/home/viste/dev-wow/launcher/target/debug/deps/launcher-7c8aeccff42c4303.d: src/main.rs
+
+src/main.rs:
